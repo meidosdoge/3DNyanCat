616dc2967dfa64c57d801cb9df218c9125812dbc
meichijo <marinachijo@gmail.com>
2020-09-04 18:11:04 -0300

    Update Testando.cs
    Código mara

--- Testando.cs
+++ Testando.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Testando : MonoBehaviour
 {
+    bool deucerto;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(deucerto)
+            print("ae");
+        else
+            print("poxa");
     }
 
     // Update is called once per frame
