fdb47d05657fe5cb682c5f344504ad76bf78c0ca
meichijo <marinachijo@gmail.com>
2020-09-14 02:18:35 -0300

    Setup do pause e da farejada bar
    - Adicionei os scripts necessários pra fazer rolar o pause
    - Adicionei a variável do pause nos scripts de movimento
    - Você ganhou a barra de limite do farejar de brinde!!!

---
+++ Player/FarejarBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarejarBar : MonoBehaviour
+{
+    public float farejaDur = 100f;
+    public float uso = 5f;
+    public float regenera = 0.5f;
+
+    public GameObject HUD_vida;
+    //public ProgressBar barraProgresso;
+
+    void Update()
+    {
+        if(!CanvasManager.jogoPausado)
+        {
+            farejaDur -= Time.deltaTime * uso;
+        }
+
+        if(farejaDur < 100)
+        {
+            farejaDur += Time.deltaTime * regenera;
+        }
+
+        //barraProgresso.current = farejaDur;
+
+        if(farejaDur <= uso)
+        {
+            //não pode farejar
+        }
+    }
+}
