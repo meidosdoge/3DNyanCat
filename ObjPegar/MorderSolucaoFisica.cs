85143b2f7188512b07210dd220f61936521d041a
André B. Manzato <abmanzato@gmail.com>
2020-09-29 16:49:17 -0300

    consertar bugs de fisica, do eleveador e tentativa de raycast da camera parada

---
+++ ObjPegar/MorderSolucaoFisica.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorderSolucaoFisica : MonoBehaviour
+{
+    //esse script é pra poder soltar o objeto e ele cair, mas só até chegar no chão
+    //fazendo com que não se mova mais dps disso e n de mais problemas de colisão
+    //o thiago vai ficar triste
+
+    Rigidbody rb;
+    bool caindo;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (rb.velocity.y < -0.1f)
+        {
+            caindo = true;
+        }
+
+        if (caindo && rb.velocity.y > -0.1f)
+        {
+            caindo = false;
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+    }
+}
