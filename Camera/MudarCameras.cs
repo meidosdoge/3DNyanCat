a73c3408935316771aa728ba831535170ca0d6ed
meichijo <60530026+meichijo@users.noreply.github.com>
2020-10-05 01:38:17 -0300

    Update MudarCameras.cs
    tirei os comentários da camera partícula, mas acho melhor desativar o grupo de partículas do que a câmera

--- Camera/MudarCameras.cs
+++ Camera/MudarCameras.cs
@@ -7,7 +7,7 @@ public class MudarCameras : MonoBehaviour
     //pega camera atual para fazer a verificação se o player está na visão dela
     Camera camera;
     //referencias de objetos para realizar a transição das cameras e executar o que precisa
-    public GameObject player, cameraPlayer;  //cameraParticulas;  não entendi a necessidade dessa aqui
+    public GameObject player, cameraPlayer, cameraParticulas;  //não entendi a necessidade dessa aqui
     //variavel que indica se está na camera fixa ou movel, para a mira conseguir funcionar de acordo
     public static bool camNoPlayer = false;
 
@@ -28,13 +28,13 @@ public class MudarCameras : MonoBehaviour
         {
             cameraPlayer.SetActive(false);
             camNoPlayer = false;
-            //cameraParticulas.SetActive(true);
+            cameraParticulas.SetActive(true);
         }
         else if (!onScreen)
         {
             cameraPlayer.SetActive(true);
             camNoPlayer = true;
-            //cameraParticulas.SetActive(false);
+            cameraParticulas.SetActive(false);
         }
     }
 }
